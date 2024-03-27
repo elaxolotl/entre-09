fd91860acaf3350acdf29e8ab855183d4f2fe514
youssefEchadySfaxi <youssefechadysfaxi@gmail.com>
2024-03-27 23:54:33 +0100

    + styling for hero section

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,22 @@ import NavBar from './Nav'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Hero from './Hero';
 import About from './About';
+import './App.css'
+
+function HeroAbout(){
+  return(
+    <div id='hero-about'>
+      <Hero/>
+      <About/>
+    </div>
+  )
+}
 
 function App() {
   return (
     <>
       <NavBar />
-      <Hero />
-      <About />
+      <HeroAbout/>
     </>
   )
 }
--- src/Hero.jsx
+++ src/Hero.jsx
@@ -2,23 +2,31 @@ import React from "react";
 import { FaChalkboardTeacher } from "react-icons/fa";
 import { IoIosArrowForward } from "react-icons/io";
 import { MdEventNote } from "react-icons/md";
+import './Hero.css'
+import FancyText from '@carefully-coded/react-text-gradient';
+
 
 export default function Hero() {
     return (
         <div id="hero" style={{ marginTop: "50px" }}>
-            <h1>Have it your way</h1>
-            <p>Tailor-made solutions for the growth and success
+            <h1>Have it <FancyText
+                    gradient={{ from: '#D4AF37', to: '#C25942', type: 'linear' }}
+                    animate
+                    animateDuration={1000}>
+                    your
+                </FancyText> way</h1>
+            <p className="hero-description">Tailor-made solutions for the growth and success
                 of your business established in Tunisia or the Eurozone
             </p>
             <div id="hero-card">
                 <h2>How can I help</h2>
                 <div className="card-subset">
                     <div>
-                        <FaChalkboardTeacher /><IoIosArrowForward />
+                        <span><FaChalkboardTeacher /><IoIosArrowForward /></span>
                         <p>I'm looking for a trainer</p>
                     </div>
                     <div>
-                        <MdEventNote /><IoIosArrowForward />
+                        <span><MdEventNote /><IoIosArrowForward /></span>
                         <p>I'm looking for assistance</p>
                     </div>
                 </div>
