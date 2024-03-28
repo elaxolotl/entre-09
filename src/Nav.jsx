5c10a029dc5a85e2a0f7d7865581d9113eb09b62
youssefEchadySfaxi <youssefechadysfaxi@gmail.com>
2024-03-28 23:03:58 +0100

    +navbar styling

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,39 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 import './Nav.css'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function NavBar() {
+
+    //adds dropdown on hover//
+
+    const [show, setShow] = useState(true);
+    const showDropdown = (e) => {
+        setShow(!show);
+    }
+    const hideDropdown = e => {
+        setShow(false);
+    }
+
+    //navbar is transparent when it is on top, then becomes white when client scrolls//
+
+    const [scrolled, setScrolled] = useState(true);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const isTop = window.scrollY < 100;
+            if (isTop !== scrolled) {
+                setScrolled(isTop);
+            }
+        };
+
+        document.addEventListener('scroll', handleScroll);
+
+        return () => {
+            document.removeEventListener('scroll', handleScroll);
+        };
+    }, [scrolled]);
     return (
-        <Navbar expand="lg" fixed="top" className="bg-body-tertiary">
-            <Container fluid>
-                <Navbar.Brand><img 
-                    src="/logo.svg" 
-                    width="30"
-                    height="30"
-                    alt="entre09"
-                    />
+        <Navbar
+            expand="lg"
+            sticky="top"
+            className={scrolled ? 'bg-transparent shadow-none' : 'bg-white shadow'}
+        >
+            <Container>
+                <Navbar.Brand>
+                    <img src="/logo.svg" width="40" height="40" alt="entre09" />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <NavDropdown title="our jobs" id="basic-nav-dropdown">
+                    <Nav className="me-auto" style={{  marginLeft: '35vh', gap:'5vh', alignItems:"center" }}>
+                        <NavDropdown
+                            class="nav-links"
+                            title={
+                                <span className="nav-links">our jobs</span>
+                            }
+                            id="basic-nav-dropdown"
+                            show={show}
+                            onMouseEnter={showDropdown}
+                            onMouseLeave={hideDropdown}
+                        >
                             <NavDropdown.Item href="#action/3.1">
                                 Trainings
                             </NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">
-                                assistance
+                                Assistance
                             </NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link>accompaniement</Nav.Link>
-                        <Nav.Link>prices</Nav.Link>
-                        <Nav.Link>contact</Nav.Link>
-                        <Nav.Link>Eng</Nav.Link>
+                        <Nav.Link class="nav-links" >Accompaniment</Nav.Link>
+                        <Nav.Link class="nav-links" >Prices</Nav.Link>
+                        <Nav.Link class="nav-links" >Contact</Nav.Link>
+                        <Nav.Link class="nav-links" style={{ marginLeft:'30vh' }}>Eng</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
-    )
+    );
 }
